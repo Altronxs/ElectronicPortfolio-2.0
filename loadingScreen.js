b8d96b5bdb96392e46886cb9487b056a5bb5534a
Altronxs <kylextron101098@gmail.com>
2026-07-22 00:25:53 -1000

    finishing up hero page

--- loadingScreen.js
+++ loadingScreen.js
@@ -4,4 +4,6 @@ window.addEventListener('load', function() {
     const spinner = document.getElementById('spinner');
     loader.classList.add('loader-hidden');
     spinner.classList.add('!h-[100%]');
+    document.getElementById('heroOne').classList.add('!bottom-[8vw]')
+    document.getElementById('heroTwo').classList.add('!bottom-[0vw]')
 });
